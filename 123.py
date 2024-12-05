9714c80f82a2c2c10707073dfb93b1926a3176e8
HwyzsyHwy <3134403288@qq.com>
2024-12-05 23:40:40 +0800

    Add files via upload

--- 123.py
+++ 123.py
@@ -33,18 +33,20 @@ selected_models = st.sidebar.multiselect("Select models to use for prediction", 
 
 # 侧边栏：输入特征值
 st.sidebar.header("Input Features")
-Moisture = st.sidebar.number_input("M", min_value=0, max_value=25, value=5)
-Ash = st.sidebar.number_input("Ash", min_value=0, max_value=35, value=5)
-Volatile_Matter = st.sidebar.number_input("VM", min_value=0, max_value=100, value=80)
-Fixed_Carbon = st.sidebar.number_input("FC", min_value=0, max_value=35, value=10)
-C = st.sidebar.number_input("C", min_value=0, max_value=70, value=50)
-H = st.sidebar.number_input("H", min_value=0, max_value=30, value=5)
-O = st.sidebar.number_input("O", min_value=20, max_value=80, value=50)
-N = st.sidebar.number_input("N", min_value=0, max_value=20, value=1)
-Particle_Size = st.sidebar.number_input("PS", min_value=0, max_value=200, value=1)
-Final_Temperature = st.sidebar.number_input("FT", min_value=200, max_value=1000, value=500)
-Heating_rate = st.sidebar.number_input("HR", min_value=0, max_value=700, value=10)
-Final_Residue = st.sidebar.number_input("FR", min_value=0, max_value=10000, value=100)
+
+# 修改数值输入控件，使其可以接受至少两位小数
+Moisture = st.sidebar.number_input("M", min_value=0.0, max_value=25.0, value=5.0, step=0.01)
+Ash = st.sidebar.number_input("Ash", min_value=0.0, max_value=35.0, value=5.0, step=0.01)
+Volatile_Matter = st.sidebar.number_input("VM", min_value=0.0, max_value=100.0, value=80.0, step=0.01)
+Fixed_Carbon = st.sidebar.number_input("FC", min_value=0.0, max_value=35.0, value=10.0, step=0.01)
+C = st.sidebar.number_input("C", min_value=0.0, max_value=70.0, value=50.0, step=0.01)
+H = st.sidebar.number_input("H", min_value=0.0, max_value=30.0, value=5.0, step=0.01)
+O = st.sidebar.number_input("O", min_value=20.0, max_value=80.0, value=50.0, step=0.01)
+N = st.sidebar.number_input("N", min_value=0.0, max_value=20.0, value=1.0, step=0.01)
+Particle_Size = st.sidebar.number_input("PS", min_value=0.0, max_value=200.0, value=1.0, step=0.01)
+Final_Temperature = st.sidebar.number_input("FT", min_value=200.0, max_value=1000.0, value=500.0, step=0.01)
+Heating_rate = st.sidebar.number_input("HR", min_value=0.0, max_value=700.0, value=10.0, step=0.01)
+Final_Residue = st.sidebar.number_input("FR", min_value=0.0, max_value=10000.0, value=100.0, step=0.01)
 
 # 将输入数据转换为 DataFrame，供模型预测使用
 input_data = pd.DataFrame({
